4040c46dbd15296745aca21e0877c72e57025a5d
So Jeong Lee <sj27419@gmail.com>
2023-06-21 23:41:48 +0900

    add inital settings

--- account/serializers.py
+++ account/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import *
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
