66fe867f4cee7c2adcd7221c91b3ecb6bcdfb3db
So Jeong Lee <sj27419@gmail.com>
2023-06-22 00:50:30 +0900

    add polls model.py

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from account.models import *
 
-# Create your models here.
+class teamPoll(models.Model):
+
+    team = models.CharField(max_length=10)
+    name = models.CharField(max_length=10) # 투표자 이름
+
+    def __str__(self):
+        return self.team
+
+
+class partPoll(models.Model):
+    part = models.CharField(max_length=10)
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.part
