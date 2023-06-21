ee7ede3540f3ed239b8791a1306c5317a714124a
So Jeong Lee <sj27419@gmail.com>
2023-06-21 21:10:12 +0900

    add account models.py

--- account/models.py
+++ account/models.py
@@ -3,25 +3,37 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import BaseUserManager
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, password, nickname, **extra_fields):
-        if not username:
-            raise ValueError('username Required!')
+    def create_user(self, userId, password, email, part, name, team):
+        if not userId:
+            raise ValueError('userId is required!')
+        if not password:
+            raise ValueError('password is required!')
+        if not email:
+            raise ValueError('email is required!')
+        if not part:
+            raise ValueError('part is required!')
+        if not name:
+            raise ValueError('name is required!')
+        if not team:
+            raise ValueError('team is required!')
 
         user = self.model(
-            username=username,
-            nickname=nickname,
-            **extra_fields
+            userId=userId,
+            email=email,
+            part=part,
+            name=name,
+            team=team,
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, password, nickname=None):
-        user = self.create_user(username, password, nickname)
+    def create_superuser(self, userId, email, password=None):
+        user = self.create_user(userId, email, password)
 
         user.is_superuser = True
-        user.is_staff = True
+        user.is_admin = True
         user.level = 0
 
         user.save(using=self._db)
@@ -45,8 +57,9 @@ class User(AbstractUser):
     )
 
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
 
+    userId = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=8)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -54,10 +67,10 @@ class User(AbstractUser):
     team = models.CharField(max_length=16, choices=teamList)
     email = models.EmailField(unique=True)
 
-    USERNAME_FIELD = 'username'
+    USERNAME_FIELD = 'userId'
     REQUIRED_FIELDS = ['name','password','email','part','team']
 
     def __str__(self):
-        return self.name
+        return self.userId
 
 ##
