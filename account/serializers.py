7f02fd7c0892affbaa15e0a066ce9d73f12b6d82
So Jeong Lee <sj27419@gmail.com>
2023-06-21 21:21:42 +0900

    fix account serializers.py

--- account/serializers.py
+++ account/serializers.py
@@ -1,15 +1,13 @@
 from rest_framework import serializers
 from .models import *
-from django.contrib.auth import get_user_model
+from django.contrib.auth import User
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-#User = get_user_model()
-
-class UserSignupSerializer(serializers.Serializer):
+class UserSignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username','password','name', 'created_at', 'updated_at', 'part', 'team']
+        fields = ['id', 'userId','password','name', 'created_at', 'updated_at', 'part', 'team']
 
     partList = (
         ('backend', 'backend'),
@@ -24,9 +22,9 @@ class UserSignupSerializer(serializers.Serializer):
         ('Hooking', 'Hooking'),
     )
 
-    username = serializers.CharField(max_length=32) # id
+    userId = serializers.CharField(max_length=32)
     password = serializers.CharField(max_length=32)
-    name = serializers.CharField(max_length=8) # 이름
+    name = serializers.CharField(max_length=8)
     email = serializers.EmailField()
     part = serializers.ChoiceField(
         choices=partList
@@ -37,32 +35,21 @@ class UserSignupSerializer(serializers.Serializer):
 
     def create(self, validated_data):
 
-        if User.objects.filter(username=validated_data['username']).exists() :
-            raise serializers.ValidationError('username exists')
+        if User.objects.filter(userId=validated_data['userId']).exists():
+            raise serializers.ValidationError('userId exists')
 
 
         else:
             user = User.objects.create(
-                username = validated_data['username'],
-                name = validated_data['name'],
-                part = validated_data['part'],
-                team = validated_data['team'],
-                email = validated_data['email'],
+                userId=validated_data['username'],
+                name=validated_data['name'],
+                part=validated_data['part'],
+                team=validated_data['team'],
+                email=validated_data['email'],
             )
             user.set_password(validated_data['password'])
             user.save()
             return user
-
-class LoginSerializer(serializers.Serializer):
- id = serializers.CharField(write_only=True, required=True)
- password = serializers.CharField(write_only=True, required=True)
-
-class RefreshToken(RefreshToken):
-    def for_user(cls, user):
-        token = super().for_user(user)
-        return token
-
-User = get_user_model()
 
 class UserLoginSerializer(serializers.Serializer):
 
@@ -97,16 +84,14 @@ class UserLoginSerializer(serializers.Serializer):
     partPoll = serializers.BooleanField(default=False)
 
     def validate(self, data):
-        username = data.get("username", None)
+        userId = data.get("userId", None)
         password = data.get("password", None)
 
-        if User.objects.filter(username=username).exists():
-            user = User.objects.get(username=username)
+        if User.objects.filter(userId=userId).exists():
+            user = User.objects.get(userId=userId)
 
             if not user.check_password(password):
                 raise serializers.ValidationError('잘못된 비밀번호입니다.')
-        # if not user.check_password(password):
-        #     raise serializers.ValidationError({"Wrong Password"})
         else:
             raise serializers.ValidationError("존재하지 않는 사용자입니다.")
 
