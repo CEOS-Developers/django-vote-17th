4c3b342966e8b4ff333ec1961e896ec553851916
haen-su <haensuya@gmail.com>
2023-06-23 21:10:59 +0900

    feat: user serializer 작성

--- api/serializers.py
+++ api/serializers.py
@@ -8,3 +8,31 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = '__all__'
 
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['name', 'login_id', 'password', 'email', 'team', 'part']
+
+    def create(self, validated_data):
+        name = validated_data.get('name')
+        login_id = validated_data.get('login_id')
+        password = validated_data.get('password')
+        email = validated_data.get('email')
+        team = validated_data.get('team')
+        part = validated_data.get('part')
+
+        user = User(
+            name=name,
+            login_id=login_id,
+            email=email,
+            team=team,
+            part=part
+        )
+
+        user.set_password(password)
+        user.save()
+
+        return user
+
+
