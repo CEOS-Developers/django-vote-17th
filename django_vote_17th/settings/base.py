6baa65fa05227aaaca99a442806569836fc2a5ae
haen-su <haensuya@gmail.com>
2023-06-26 16:46:58 +0900

    fix: cors origin 설정 변경

--- django_vote_17th/settings/base.py
+++ django_vote_17th/settings/base.py
@@ -169,6 +169,14 @@ SWAGGER_SETTINGS = {
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ORIGIN_WHITELIST = (
+    "https://takgyun.store",
+    "https://www.takgyun.store",
+    "http://localhost:3000",
+    "http://127.0.0.1:8000"
+)
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
