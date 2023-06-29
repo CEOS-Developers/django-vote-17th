e3636cc52f186c4e85b1abe115275e9915a994ce
haen-su <haensuya@gmail.com>
2023-06-30 01:39:10 +0900

    fix: cookie httpOnly false

--- api/views.py
+++ api/views.py
@@ -109,7 +109,8 @@ class SignIn(APIView):
             )
 
             # response.set_cookie("access_token", access_token, httponly=True)
-            response.set_cookie("refresh_token", refresh_token, httponly=True, domain="takgyun.store", path="/")
+            response.set_cookie("refresh_token", refresh_token)
+
             return response
         else:
             return Response(
